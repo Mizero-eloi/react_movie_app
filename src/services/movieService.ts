48e2026c4427bdfbf31e91e0e642f0944c918686
Mizero-eloi <80544579+Mizero-eloi@users.noreply.github.com>
2022-08-23 09:56:49 +0200

    added top rated movies

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -28,3 +28,12 @@ export const getOnTheAirTv = async () => {
   console.log("The response", response);
   return response;
 };
+
+export const getTopRated = async () => {
+  const response = await http.get(
+    `${env.VITE_MOVIE_APP_API_URL}movie/top_rated?api_key=${env.VITE_API_KEY}`
+  );
+
+  console.log("The response", response);
+  return response;
+};
