b775be6dc31c3efe1a187502cce92d47bdd31ac8
Mizero-eloi <80544579+Mizero-eloi@users.noreply.github.com>
2022-08-24 16:18:08 +0200

    adjusted width

--- src/components/RightSideBar.tsx
+++ src/components/RightSideBar.tsx
@@ -6,6 +6,7 @@ import useAuthStore from "../store/authStore";
 import { createOrGetUser } from "./../services/userService";
 import { getTopRated } from "../services/movieService";
 import { IMovie } from "../types";
+import { Link } from "react-router-dom";
 
 const RightSideBar = () => {
   const { userProfile } = useAuthStore();
@@ -51,23 +52,25 @@ const RightSideBar = () => {
         <h2 className="text-xl">Top rated</h2>
         <div className="mt-2 flex flex-col gap-6 h-[638px] overflow-y-scroll noScrollbar">
           {topRatedMovies.slice(0, 4).map((m) => (
-            <div className="flex gap-3">
-              <img
-                src={`https://image.tmdb.org/t/p/original${m.poster_path}`}
-                alt={m.title}
-                className="w-1/2 h-40 rounded-lg"
-              />
-              <div className="w-1/2 flex flex-col gap-4">
-                <h2 className="text-sm font-medium">{m.title}</h2>
-                <p className="text-xs text-gray-300">
-                  {m.overview.substring(0, 40) + "..."}
-                </p>
-                <div className="w-20  border border-blue-400 rounded-full flex gap-3 justify-center items-center">
-                  {m.vote_average}
-                  <AiFillStar />
+            <Link to={`/movie/${m.id}`}>
+              <div className="flex gap-3">
+                <img
+                  src={`https://image.tmdb.org/t/p/original${m.poster_path}`}
+                  alt={m.title}
+                  className="w-1/2 h-40 rounded-lg"
+                />
+                <div className="w-1/2 flex flex-col gap-4">
+                  <h2 className="text-sm font-medium">{m.title}</h2>
+                  <p className="text-xs text-gray-300">
+                    {m.overview.substring(0, 40) + "..."}
+                  </p>
+                  <div className="w-20  border border-blue-400 rounded-full flex gap-3 justify-center items-center">
+                    {m.vote_average}
+                    <AiFillStar />
+                  </div>
                 </div>
               </div>
-            </div>
+            </Link>
           ))}
         </div>
       </div>
